1831f510f602e280bd938613496ebee00a87a66e
arpan <arpanriju2000@gmail.com>
2020-02-02 23:36:16 +0530

    comments added

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from classes.operation import operation
 
 if __name__ == '__main__':
 
+    # conversions
+
     if sys.argv[1] == 'c' or sys.argv[1] == 'C':
         if sys.argv[2] == 'd' or sys.argv[2] == 'D':
             conversion(sys.argv[2], sys.argv[3])
@@ -15,6 +17,8 @@ if __name__ == '__main__':
         if sys.argv[2] == 'h' or sys.argv[2] == 'H':
             conversion(sys.argv[2], sys.argv[3])
 
+    # operations
+
     if sys.argv[1] == 'o' or sys.argv[1] == 'O':
         if sys.argv[2] == 'd' or sys.argv[2] == 'D':
             operation(sys.argv[2], sys.argv[3], sys.argv[4])
@@ -24,4 +28,3 @@ if __name__ == '__main__':
             operation(sys.argv[2], sys.argv[3], sys.argv[4])
         if sys.argv[2] == 'h' or sys.argv[2] == 'H':
             operation(sys.argv[2], sys.argv[3], sys.argv[4])
-
